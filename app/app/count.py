5532e07643876c1521965ab8161cc42ce5eb8296
Marho <joborfrederick0214@gmail.com>
2024-12-25 13:21:18 +0100

    configure db and added test for wait for db

--- app/app/count.py
+++ app/app/count.py
@@ -10,6 +10,7 @@ def add(x, y):
 
     return x + y
 
+
 def subtract(x, y):
-    
-    return x-y 
+
+    return x-y
--- app/app/test.py
+++ app/app/test.py
@@ -6,21 +6,21 @@ from django.test import SimpleTestCase
 
 from app import count
 
+
 class CalcTests(SimpleTestCase):
     """
     Test the calc module
     """
 
-
-    def test_add_number(self):
+    def add_number(self):  # omitted test prefix
         """
             Test adding numnbers together
         """
-        res = count.add(5,6)
+        res = count.add(5, 6)
 
         self.assertEqual(res, 11)
 
-    def test_subtract_numbers(self):
+    def subtract_numbers(self):
         """ subtracting number """
 
         res = count.subtract(10, 5)
